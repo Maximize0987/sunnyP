56509c7217802afd0c20e3dc6c011b5f2dec10cc
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-02-21 12:25:06 -0700

    Update interface.py.   test tune .6 delay

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -39,7 +39,7 @@ class CarInterface(CarInterfaceBase):
     else:
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
-      ret.steerActuatorDelay = 0.42          #       ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
+      ret.steerActuatorDelay = 0.60          #       ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
       ret.steerLimitTimer = 0.9       #   ret.steerLimitTimer = 0.4
 
       if 0x23 in fingerprint[0]:  # Detect if ZSS is present
