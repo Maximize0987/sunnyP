f805ca56fadf85c13cfe96afb57e301b43c5ce4f
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-02-25 17:15:16 -0700

    Update interfaces.py   lower kf

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -392,7 +392,7 @@ class CarInterfaceBase(ABC):
     tune.init('torque')
     tune.torque.useSteeringAngle = use_steering_angle
     tune.torque.kp = 0.8   #   1.0
-    tune.torque.kf = 1.014   #    1.0
+    tune.torque.kf = 1.01   #    1.0
     tune.torque.ki = 0.1  #    0.1
     tune.torque.friction = params['FRICTION']
     tune.torque.latAccelFactor = params['LAT_ACCEL_FACTOR']
