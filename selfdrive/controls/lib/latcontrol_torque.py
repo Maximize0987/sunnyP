bdbc597a39ebfe1e76b2bab173eeb7e9f91ea4a3
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-03-26 20:46:47 -0600

    Update latcontrol_torque.py. roll 1.0

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -32,7 +32,7 @@ LAT_PLAN_MIN_IDX = 5
 
 ERROR_PERCENT = 1.0
 
-rollEdit = 1.2
+rollEdit = 1.0
 
 def get_predicted_lateral_jerk(lat_accels, t_diffs):
   # compute finite difference between subsequent model_data.acceleration.y values
