698fa19254137bf52e2904bc739dca7b5d554489
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-07-17 10:06:37 -0600

    Update calibrationd.py.  raise yaw offset

--- selfdrive/locationd/calibrationd.py
+++ selfdrive/locationd/calibrationd.py
@@ -37,7 +37,7 @@ RPY_INIT = np.array([0.0,0.0,0.0])
 WIDE_FROM_DEVICE_EULER_INIT = np.array([0.0, 0.0, 0.0])
 HEIGHT_INIT = np.array([1.22])
 
-RPY_OFFSET = np.array([0.0,0.0,0.00218])      # attempt to keep vehicle away from left lane line
+RPY_OFFSET = np.array([0.0,0.0,0.00262])      # attempt to keep vehicle away from left lane line
 
 # These values are needed to accommodate the model frame in the narrow cam
 if HARDWARE.get_device_type() == 'mici':
