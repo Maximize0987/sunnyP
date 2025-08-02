584a7ba74ca5c12b71fcd2fbc96f06cba41b4d5c
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-08-02 06:30:37 -0600

    Update interface.py     raise steer limittimer

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -40,7 +40,7 @@ class CarInterface(CarInterfaceBase):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
       ret.steerActuatorDelay = 0.135        #       ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
-      ret.steerLimitTimer = 0.2       #   ret.steerLimitTimer = 0.4
+      ret.steerLimitTimer = 0.6       #   ret.steerLimitTimer = 0.4
 
       if 0x23 in fingerprint[0]:  # Detect if ZSS is present
         ret.spFlags |= ToyotaFlagsSP.SP_ZSS.value
