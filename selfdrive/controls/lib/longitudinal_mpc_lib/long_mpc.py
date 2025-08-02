78a03867b60b39a3d7ef1114cc51e2d213669c66
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-08-02 09:54:27 -0600

    new follow dist

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -38,7 +38,7 @@ V_EGO_COST = 0.
 A_EGO_COST = 0.
 J_EGO_COST = 5.0
 A_CHANGE_COST = 200.
-DANGER_ZONE_COST = 10.   # old = 100
+DANGER_ZONE_COST = 1.   # old = 100
 CRASH_DISTANCE = .25
 LEAD_DANGER_FACTOR = 0.75
 LIMIT_COST = 1e6
@@ -55,7 +55,7 @@ T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 COMFORT_BRAKE = 2.5    # old = 2.5
-STOP_DISTANCE = 9.0   # old = 6.0
+STOP_DISTANCE = 6.0
 
 def get_jerk_factor(personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
@@ -74,13 +74,13 @@ def get_jerk_factor(personality=custom.LongitudinalPersonalitySP.standard):
 
 def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
-    return 1.75
+    return 1.4
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    return 1.45
+    return 1.2
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 1.25
+    return 1.0
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    return 0.9    # old = 1.0
+    return 0.65  # old = 1.0
   elif personality==custom.LongitudinalPersonalitySP.overtake:
     return 0.1     # old = 0.25
   else:
