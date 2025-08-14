b86498396ed08ffaf8fe38bb711646d8f4fc74cc
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-08-14 11:20:50 -0700

    Update radar_interface.py. radar offset part 2

--- selfdrive/car/toyota/radar_interface.py
+++ selfdrive/car/toyota/radar_interface.py
@@ -6,7 +6,7 @@ from openpilot.selfdrive.car.interfaces import RadarInterfaceBase
 import numpy as np
 
 xdistance = np.array([0, 230], dtype=np.float32)
-yoffset = np.array([0.0, 0.4], dtype=np.float32)
+yoffset = np.array([0.0, 0.8], dtype=np.float32)
 
 def _create_radar_can_parser(car_fingerprint):
   if car_fingerprint in TSS2_CAR:
@@ -83,6 +83,13 @@ class RadarInterface(RadarInterfaceBase):
             self.pts[ii] = car.RadarData.RadarPoint.new_message()
             self.pts[ii].trackId = self.track_id
             self.track_id += 1
+          xinput = int(cpt['LONG_DIST'])
+          yinput = float(cpt['LAT_DIST'])
+          yprint = round(yinput,2)
+          yoffsetoutput = float(np.interp(xinput, xdistance, yoffset)) 
+          yOO = (round(yoffsetoutput,2))
+          #print(f"Long: {xinput} Lat: {yprint} Score: {score}")
+          cpt['LAT_DIST'] = cpt['LAT_DIST'] - yOO
           self.pts[ii].dRel = cpt['LONG_DIST']  # from front of car
           self.pts[ii].yRel = -cpt['LAT_DIST']  # in car frame's y axis, left is positive
           self.pts[ii].vRel = cpt['REL_SPEED']
